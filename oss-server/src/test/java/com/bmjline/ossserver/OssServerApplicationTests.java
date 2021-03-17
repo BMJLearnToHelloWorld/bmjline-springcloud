ec4c1c2e37e552425851df0baa00630b6158e137
BMJ <BernieMJ1012@gmail.com>
2021-03-17 23:03:28 +0800

    add docker compose file

---
+++ oss-server/src/test/java/com/bmjline/ossserver/OssServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bmjline.ossserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OssServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
